afb30f1c3a40c5b0db72bc2c136f194dea538234
Joe Heck <heckj@mac.com>
2021-04-08 15:14:47 -0700

    broken build with missing dependency

--- Documentation/Sources/Kalimba/Kalimba.swift
+++ Documentation/Sources/Kalimba/Kalimba.swift
@@ -1,11 +1,30 @@
+// extension Sequence {
+//   public func kalimbaOrdered() -> [Element] {
+//     var kalimba: [Element] = []
+//     kalimba.reserveCapacity(underestimatedCount)
+//     var insertAtStart = false
+//     for element in self {
+//       if insertAtStart {
+//         kalimba.insert(element, at: 0)
+//       } else {
+//         kalimba.append(element)
+//       }
+//       insertAtStart.toggle()
+//     }
+//     return kalimba
+//   }
+// }
+
+import Collections
+
 extension Sequence {
-  public func kalimbaOrdered() -> [Element] {
-    var kalimba: [Element] = []
+  public func kalimbaOrdered() -> Deque<Element> {
+    var kalimba: Deque<Element> = []
     kalimba.reserveCapacity(underestimatedCount)
     var insertAtStart = false
     for element in self {
       if insertAtStart {
-        kalimba.insert(element, at: 0)
+        kalimba.prepend(element)
       } else {
         kalimba.append(element)
       }
