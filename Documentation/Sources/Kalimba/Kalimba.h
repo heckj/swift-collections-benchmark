066c43b5485856f16155e4b3bc2787e5e53770ce
Joe Heck <heckj@mac.com>
2021-04-08 14:57:21 -0700

    removing unneeded header

--- Documentation/Sources/Kalimba/Kalimba.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for Minerva.
-FOUNDATION_EXPORT double KalimbaVersionNumber;
-
-//! Project version string for Minerva.
-FOUNDATION_EXPORT const unsigned char KalimbaVersionString[];
